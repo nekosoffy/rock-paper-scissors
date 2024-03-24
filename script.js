c32a4c7c29332c718d0a2f19e78e6dac09243c26
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-03-24 13:22:36 -0300

    Create function to get player choice

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function getPlayerChoice() {
+    let showPrompt = true;
+    while (showPrompt === true) {
+        let choice = prompt("Please, pick rock, paper or scissors.")
+        if (typeof choice === "string") {
+            if (choice.toLowerCase() === "rock" ) {
+                showPrompt = false;
+                return "Rock";
+            } else if (choice.toLowerCase() === "paper") {
+                showPrompt = false;
+                return "Paper";
+            } else if (choice.toLowerCase() === "scissors") {
+                showPrompt = false;
+                return "Scissors";
+            } else {
+            alert("That's an invalid option.");
+            }
+        } else {
+            alert("That's an invalid option.");
+        }
+    }
+}
