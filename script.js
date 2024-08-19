7618578fbc14d534c8aafbd06010e912641fccbb
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-08-19 14:43:42 -0300

    Add UI instead of using prompts and the console

--- script.js
+++ script.js
@@ -1,71 +1,86 @@
+let playerTracker = 0;
+let computerTracker = 0;
+let roundTracker = 1;
+let playerSelection;
+let computerSelection;
+
+const round_div = document.body.appendChild(document.createElement("div"));
+const score_div = document.body.appendChild(document.createElement("div"));
+const buttons_div = document.body.appendChild(document.createElement("div"));
+
 function getComputerChoice() {
     let choiceNumber = Math.floor(Math.random() * 3) + 1;
     if (choiceNumber === 1) {
         return "rock";
     } else if (choiceNumber === 2) {
         return "paper";
-    } else {
-        return "scissors";
-    }
+    } return "scissors";
 }
 
 function getPlayerChoice() {
-    let choice;
-    while (true) {
-        choice = prompt("Please, pick rock, paper, or scissors.");
-        if (typeof choice === "string") {
-            choice = choice.toLowerCase();
-            if (choice === "rock" || choice === "paper" || choice === "scissors") {
-                return choice;
-            } else {
-                alert("That's an invalid option.");
-            }
-        } else {
-            alert("That's an invalid option.");
-        }
-    }
+    const rock_btn = buttons_div.appendChild(document.createElement("button"));
+    const paper_btn = buttons_div.appendChild(document.createElement("button"));
+    const scissors_btn = buttons_div.appendChild(document.createElement("button"));
+
+    rock_btn.textContent = "Rock";
+    paper_btn.textContent = "Paper";
+    scissors_btn.textContent = "Scissors";
+
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerSelection = button.textContent.toLowerCase();
+            buttons_div.replaceChildren();
+            score_div.replaceChildren();
+            playRound();
+        });
+    });
 }
 
-let playerTracker = 0;
-let computerTracker = 0;
-let playerSelection;
-let computerSelection;
+function playRound() {
+    const roundText = document.createElement("p");
+    const playerScore = document.createElement("p");
+    const computerScore = document.createElement("p");
 
-function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        result = `Both player and computer picked ${playerSelection}. It's a draw!`;
+        roundText.textContent = `Both player and computer picked "${playerSelection}". It's a draw! (${roundTracker}/5)`;
+        ++roundTracker;
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors" ) ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper") 
     ) {
-        result = `Computer picked ${computerSelection}. You won!`;
+        roundText.textContent = `Computer picked "${computerSelection}". You won! (${roundTracker}/5)`;
         ++playerTracker;
+        ++roundTracker;
     } else {
-        result = `Computer picked ${computerSelection}. You lost!`;
+        roundText.textContent = `Computer picked "${computerSelection}". You lost! (${roundTracker}/5)`;
         ++computerTracker;
+        ++roundTracker;
     }
-    console.log(result);
-    console.log(`Current player score: ${playerTracker}`);
-    console.log(`Current computer score: ${computerTracker}`);
+
+    playerScore.textContent = `Current player score: ${playerTracker}`
+    computerScore.textContent = `Current computer score: ${computerTracker}`;
+    round_div.appendChild(roundText);
+    score_div.appendChild(playerScore);
+    score_div.appendChild(computerScore);
+
+    if (roundTracker <= 5) {
+        playGame();
+    } else {
+        const result = document.createElement("p");
+        if (playerTracker > computerTracker && roundTracker === 6) {
+            result.textContent = "The game finished! You won!";
+        } else if (playerTracker < computerTracker && roundTracker === 6) {
+            result.textContent = "The game finished! You lost!";
+        };
+        score_div.appendChild(result);
+    }
 }
 
-async function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection,computerSelection);
-        await delay(1000);
+function playGame() {
+        computerSelection = getComputerChoice();
+        getPlayerChoice();
     }
-    if (playerTracker > computerTracker) {
-        console.log("The game finished! You won!");
-        } else {
-        console.log("The game finished! You lost!");
-    }
-}
-
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
+    
 playGame();
